60b4d50267d27af5cc5970155129f8c08a2f9c17
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-18 00:11:21 +0530

    implement render of NumberGroupRenderer

--- numbers.js
+++ numbers.js
@@ -11,13 +11,53 @@ class NumberGroupController {
     this.#numberGroup.add(1);
     this.#numberGroup.add(3);
     this.#numberGroup.add(2);
+    this.#numberGroupView.render(this.#numberGroup);
 
     console.log(this.#numberGroup.numbers);
     console.log(this.#numberGroup.max);
   }
 }
 
-class NumberGroupView {}
+class NumberGroupView {
+  #parentContainer;
+  #container;
+
+  constructor(title, parentContainer) {
+    this.#parentContainer = parentContainer;
+    this.#container = this.#initialize(title);
+  }
+
+  #initialize(title) {
+    const groupElement = document.createElement("div");
+    groupElement.classList.add("number-group");
+
+    const titleElement = document.createElement("h3");
+    titleElement.innerText = title;
+
+    const container = document.createElement("ul");
+    container.classList.add("numbers");
+
+    groupElement.appendChild(titleElement);
+    groupElement.appendChild(container);
+    this.#parentContainer.appendChild(groupElement);
+
+    return container;
+  }
+
+  #addNumber(number) {
+    const numberElement = document.createElement("li");
+    numberElement.innerText = number;
+    this.#container.appendChild(numberElement);
+  }
+
+  render(numberGroup) {
+    [...this.#container.children].forEach((child) =>
+      this.#container.removeChild(child)
+    );
+
+    numberGroup.numbers.forEach((number) => this.#addNumber(number));
+  }
+}
 
 class NumberGroup {
   #numbers;
@@ -43,10 +83,10 @@ class NumberGroup {
 }
 
 window.onload = () => {
-  console.log("hello world");
+  const page = document.querySelector("#page");
 
   const numberGroup = new NumberGroup();
-  const numberGroupView = new NumberGroupView();
+  const numberGroupView = new NumberGroupView("Numbers", page);
   const numberGroupController = new NumberGroupController(
     numberGroup,
     numberGroupView
