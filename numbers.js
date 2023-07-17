3563384ddd4e3ffc6c5be52c15c259de29461097
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-17 22:52:14 +0530

    add feature to NumberGroup: add, numbers, max

--- numbers.js
+++ numbers.js
@@ -1,10 +1,46 @@
 class NumberGroupController {
-  start() {}
+  #numberGroup;
+  #numberGroupView;
+
+  constructor(numberGroup, numberGroupView) {
+    this.#numberGroup = numberGroup;
+    this.#numberGroupView = numberGroupView;
+  }
+
+  start() {
+    this.#numberGroup.add(1);
+    this.#numberGroup.add(3);
+    this.#numberGroup.add(2);
+
+    console.log(this.#numberGroup.numbers);
+    console.log(this.#numberGroup.max);
+  }
 }
 
 class NumberGroupView {}
 
-class NumberGroup {}
+class NumberGroup {
+  #numbers;
+
+  constructor() {
+    this.#numbers = [];
+  }
+
+  add(number) {
+    this.#numbers.push(number);
+  }
+
+  get numbers() {
+    return [...this.#numbers];
+  }
+
+  get max() {
+    return this.#numbers.reduce(
+      (max, number, index) => (number > max.number ? { number, index } : max),
+      { number: -Infinity, index: -1 }
+    );
+  }
+}
 
 window.onload = () => {
   console.log("hello world");
